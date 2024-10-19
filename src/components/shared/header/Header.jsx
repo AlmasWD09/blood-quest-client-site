febe2569f7f0aac9d5ecdfa97c54ac27cf1125cb
almas hossain <almashossain7384@gmail.com>
2024-10-20 02:30:26 +0600

    navbar logout button add and fixed some bug

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -13,24 +13,26 @@ const Header = () => {
     const navigate = useNavigate();
     const [isOpen, setIsOpen] = useState(true);
     const [navbar, setNavbar] = useState(false)
-
+    const [menuOpen, setMenuOpen] = useState(false)
     const handleNavigaet = () => {
         navigate('/')
     }
-    
+
     // background color add in navbar scroll
     const changeBackground = () => {
-      if (window.scrollY >= 32) {
-        setNavbar(true)
-      }
-      else { setNavbar(false) }
+        if (window.scrollY >= 32) {
+            setNavbar(true)
+        }
+        else { setNavbar(false) }
     }
     window.addEventListener('scroll', changeBackground)
 
-  
+    const handleMenu = () => {
+        setMenuOpen(!menuOpen)
+    }
     return (
         <>
-            <header className={navbar ? 'bg-primaryGray sticky top-0 left-0 z-[99999] shadow-md w-full   py-4 lg:py-6  overflow-y-hidden' : 'sticky top-0 left-0 z-[99999] shadow-md w-full py-4 lg:py-6  overflow-y-hidden '}>
+            <header className={navbar ? 'bg-primaryGray sticky top-0 left-0 z-[99999] shadow-md w-full   py-4 lg:py-6  overflow-visible' : 'sticky top-0 left-0 z-[99999] shadow-md w-full py-4 lg:py-6  overflow-visible '}>
                 <Container>
                     <div className=" flex items-center">
                         <nav className="relative container flex justify-between items-center">
@@ -81,28 +83,47 @@ const Header = () => {
                                         <Button text={"Fundings"} />
 
                                     </NavLink>
-                                    {
-                                        user && <NavLink to='/dashboard' >
-                                            <Button text={"Dashboard"} />
-                                        </NavLink>
-                                    }
+
 
                                     {/* navbar Sign Up and login button */}
-                                    <div className="md:w-[20%] ">
-                                        <div className="flex flex-col gap-4 pb-4">
+                                    <div className=" bg-secondery/50 rounded-md py-3">
+                                        <div className="flex flex-col items-center gap-4">
+                                            {
+                                                user && <div>
+                                                    <Link to='/dashboard' className="text-xl  font-semibold font-lato ">Dashboard</Link>
+                                                </div>
+                                            }
+
                                             <Link to='/login'>
-                                                <button className="w-full text-lg font-semibold font-lato py-2 mt-2">Login</button>
+                                                <button className="text-lg  font-semibold font-lato">Login</button>
                                             </Link>
                                             {
-                                                user ? 
-                                                    <button
-                                                    onClick={logOut}
-                                                    className="w-full px-5 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-primary rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Logout</button>
+                                                user ? <div onClick={handleMenu} className="w-full flex justify-center focus:outline-none cursor-pointer relative group">
+                                                    <div className="w-10 h-10 overflow-hidden border-2 border-gray-400 rounded-full">
+                                                        <img
+                                                            src={user?.photoURL}
+                                                            className="object-cover w-full h-full "
+                                                            alt={user?.displayName}
+                                                        />
+                                                    </div>
+                                                    {
+                                                        menuOpen && <div className="absolute  flex flex-col justify-center bg-gray-100 rounded-md px-4 py-3 left-1 top-12 z-50 space-y-3">
+                                                         
+                                                            <p className="uppercase">{user?.displayName}</p>
+                                                            <p>{user?.email}</p>
+                                                            <button
+                                                                onClick={logOut}
+                                                                className="w-full bg-gray-200 rounded-md py-1">Logout</button>
+                                                          
+                                                        </div>
+                                                    }
+                                                </div>
                                                     :
                                                     <Link to='/sign-up'>
                                                         <button className="w-full px-5 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-primary rounded-md hover:bg-secondery focus:outline-none focus:bg-secondery">Sign Up</button>
                                                     </Link>
                                             }
+
                                         </div>
                                     </div>
                                 </div>
@@ -135,8 +156,8 @@ const Header = () => {
                                             <Link to='/dashboard' className="text-xl font-semibold font-lato text-primary">Dashboard</Link>
                                         </div>
                                     }
-                            
-                                     <Link to='/login'>
+
+                                    <Link to='/login'>
                                         <button className="text-lg font-semibold font-lato py-2">Login</button>
                                     </Link>
                                     {
@@ -148,12 +169,19 @@ const Header = () => {
                                                     alt={user?.displayName}
                                                 />
                                             </div>
+                                            <div className="absolute group-hover:flex flex-col transform scale-y-0 group-hover:scale-y-100 origin-top ease-in transition duration-150 bg-gray-50 rounded-md px-4 py-3 right-2 top-10 z-50 space-y-2">
+                                                <p className="uppercase">{user?.displayName}</p>
+                                                <p>{user?.email}</p>
+                                                <button
+                                                    onClick={logOut}
+                                                    className="w-full bg-gray-200 rounded-md py-1">Logout</button>
+                                            </div>
                                         </div>
                                             :
                                             <Link to='/sign-up'>
                                                 <button className="w-full px-5 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-primary rounded-md hover:bg-secondery focus:outline-none focus:bg-secondery">Sign Up</button>
                                             </Link>
-                                    } 
+                                    }
 
                                 </div>
                             </div>
